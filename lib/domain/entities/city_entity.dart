f3e3c358c2977b8b97c1b32d57eb75351df1ecdd
MariaGabriele00 <gabi2015maria2015@gmail.com>
2023-08-08 15:14:24 -0300

    feat: entidade [CityEntity] criada

---
+++ lib/domain/entities/city_entity.dart
@@ -0,0 +1,15 @@
+class CityEntity {
+  final String name;
+  final String state;
+  final String country;
+  final double lon;
+  final double lag;
+
+  CityEntity({
+    required this.name,
+    required this.state,
+    required this.country,
+    required this.lon,
+    required this.lag,
+  });
+}
