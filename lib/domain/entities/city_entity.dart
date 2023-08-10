65736e02b53ad7ab3d583fdfa864e06f4066361a
MariaGabriele00 <gabi2015maria2015@gmail.com>
2023-08-10 15:14:58 -0300

    feat: add [Equatable] na entidade de cidade

--- lib/domain/entities/city_entity.dart
+++ lib/domain/entities/city_entity.dart
@@ -1,15 +1,26 @@
-class CityEntity {
+import 'package:equatable/equatable.dart';
+
+class CityEntity extends Equatable {
   final String name;
   final String state;
   final String country;
   final double lon;
   final double lag;
 
-  CityEntity({
+  const CityEntity({
     required this.name,
     required this.state,
     required this.country,
     required this.lon,
     required this.lag,
   });
+
+  @override
+  List<Object?> get props => [
+        name,
+        state,
+        country,
+        lon,
+        lag,
+      ];
 }
